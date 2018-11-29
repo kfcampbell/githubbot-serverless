f8b73fce51c33afac0f129955714d4a3f0c9165e
Keegan Campbell <kfcampbell@Keegans-MacBook-Pro.local>
2018-11-28 22:11:36 -0800

    Initial commit.

---
+++ TestTrigger/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function run(context, req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        context.log('JavaScript HTTP trigger function processed a request.');
+        if (req.query.name || (req.body && req.body.name)) {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: "Hello! " + (req.query.name || req.body.name) + ' motherfucker'
+            };
+        }
+        else {
+            context.res = {
+                status: 400,
+                body: "Please pass a name on the query string or in the request body"
+            };
+        }
+        context.done();
+    });
+}
+exports.run = run;
+;
---
+++ readme.md
@@ -0,0 +1,6 @@
+# githubbot
+
+## todo
+    - pick github api npm package or roll own
+        - bindings for typescript for npm package???
+    - implement the real thing
