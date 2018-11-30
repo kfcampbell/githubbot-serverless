5d9ebb7115c4cd4030db748e5d595a72f195ba21
Keegan Campbell <kfcampbell@Keegans-MacBook-Pro.local>
2018-11-29 19:20:26 -0800

    Basic file updating works.

--- TestTrigger/index.ts
+++ TestTrigger/index.ts
@@ -8,18 +8,27 @@ export async function run(context: any, req: any) {
     });
 
     // note: make sure to use the following callback arguments otherwise everything is null
-    const repo = gh.getRepo('kfcampbell', 'GithubBotRepo');
-    const commit = repo.getCommit('4663022e221663471d9466a618f6401912088692', (error: any, result: any, request: any) => {
-        context.log('got a commit maybe? ' + result);
+    const repo = gh.getRepo('kfcampbell', 'githubbot-serverless');
+
+    repo.getSha('master', 'updates/daily_updates.txt', (error: any, result: any, request: any) => {
+        context.log('got the sha!' + result);
+        if (error) {
+            context.log('error getting sha! ' + error);
+            return;
+        }
+
+        repo.getBlob(result.sha, (error: any, result: any, request: any) => {
+            context.log('found the blob!' + result);
+
+            const newContents = `This page was last updated on: ${new Date().toUTCString()}`
+            repo.writeFile('master', 'updates/daily_updates.txt', newContents, 'This is a commit message!', {
+                encode: true
+            }, (error: any, result: any, request: any) => {
+                context.log('maybe we wrote a file???' + result);
+            });
+        });
     });
 
-    // todo: figure out how to get the content of a specific file
-    // ideas: get sha and then get blob. something might have to be base64 encoded or decoded
-    // note: need to create directory in repo to actually house the files.
-    // have to remember to ignore when deploying to function.
-    const sha = repo.getSha('updates/daily_updates.txt', (error: any, result: any, request: any) => {
-        context.log('got the sha!' + result);
-    });
 
     context.res = {
         body: 'when i met your mom she told me i was handsome'
